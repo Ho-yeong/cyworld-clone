e4bc094c5e774fc29f531b92fb0603b7ccf99a27
Simon <ghdud0503@gmail.com>
2020-06-07 02:47:36 +0900

    frontEnd

---
+++ src/main/java/com/practice/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.practice.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("defaultPic","/img/profile_pic.jpg");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+	public ModelAndView profile() {
+		ModelAndView mv = new ModelAndView("profile");
+		mv.addObject("defaultPic","/img/profile_pic.jpg");
+		
+		return mv;
+	}
+	
+}
